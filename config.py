e7161c6e61c4d03ad801ea6f37c6869a86be3937
yzhou <zhouyang2640@gmail.com>
2016-04-13 11:04:02 -0500

    Add chaning password

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # MAIL settings
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
